c59c1699eaa793887c60611f8c8c0b57431200c5
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2025-09-13 22:19:56 +0100

    removed default case from switch options

--- main.c
+++ main.c
@@ -5,20 +5,10 @@
 #include "elf.c"
 
 int main(int argc, char* argv[]){
-    /*
-    enum{
-        PARSE_SECTION_HEADER,
-        PARSE_SECTION_TABLE,
-        PARSE_TEXT_SECTION,
-        PARSE_ALL
-    }mode;
-    was usless i just wanted to add this cause i thought it looked cool lol
-    */
-
     int option;
     if(argc<3){
-        printf("pls pass in the required arguments\n");
-        exit(1);
+        fprintf(stderr,"usage : %s [-PTS] [FILE]",argv[0]);
+        exit(EXIT_FAILURE);
     }
     char *file_name=argv[2];
 
@@ -28,26 +18,22 @@ int main(int argc, char* argv[]){
         switch(option){
 
             case 'P':
-            print_elf_headers(file_name);
-            break;
+                print_elf_headers(file_name);
+                break;
 
             case 'T': 
-            parse_text_section(file_name);
-            break;
+                parse_text_section(file_name);
+                break;
 
             case 'S':
-            parse_section_table(file_name);
-            break;
+                parse_section_table(file_name);
+                break;
 
             case 'A':
-            print_elf_headers(file_name);
-            parse_section_table(file_name);
-            parse_text_section(file_name);
-            break;
-
-            default:
-            fprintf(stderr,"usage : %s [-PTS] [FILE]",argv[0]);
-            exit(EXIT_FAILURE);
+                print_elf_headers(file_name);
+                parse_section_table(file_name);
+                parse_text_section(file_name);
+                break;
         }
   
     }    
