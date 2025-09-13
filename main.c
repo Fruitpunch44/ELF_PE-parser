c7f9f83ef5c1339a4245e48e007f0ece3bc66fee
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2025-09-13 22:52:34 +0100

    fixed a bug that mad the  program remain in an infinite loop after pasing arguments

--- main.c
+++ main.c
@@ -2,19 +2,28 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-#include "elf.c"
+#include "my_elf.h"
+
+void print_usage(const char* program_name) {
+    fprintf(stderr, "Usage: %s [-PTSA] [FILE]\n", program_name);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "  -P    Print ELF headers\n");
+    fprintf(stderr, "  -T    Parse text section\n");
+    fprintf(stderr, "  -S    Parse section table\n");
+    fprintf(stderr, "  -A    All of the above\n");
+}
 
 int main(int argc, char* argv[]){
     int option;
     if(argc<3){
-        fprintf(stderr,"usage : %s [-PTS] [FILE]",argv[0]);
+       print_usage(argv[0]);
         exit(EXIT_FAILURE);
     }
     char *file_name=argv[2];
 
     //parse command line args,man i miss python wtf is this
     //LINUX specific
-    while((option = getopt(argc,argv,"PTSA"))!=1){
+    while((option = getopt(argc,argv,"PTSA"))!=-1){
         switch(option){
 
             case 'P':
